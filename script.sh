b5baaef688ca461f2cc713b57a1b4b3862164e41
Mihai Alexandru Teodor <40050793+mihaialexandruteodor@users.noreply.github.com>
2020-12-03 19:26:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,5 @@
+## git
+
 sudo apt-get install git
 
 ## i3
@@ -29,3 +31,37 @@ sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
+
+## nano
+echo "set constantshow" >> ~/.nanorc
+echo "set linenumbers" >> ~/.nanorc
+echo "include \"/usr/share/nano/*.nanorc\"" >> ~/.nanorc
+
+# enable snaps
+sudo apt update
+sudo apt install snapd
+
+#cmake
+sudo snap install cmake --classic
+
+#vscode
+sudo snap install code --classic
+
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt install sublime-text
+
+# spotify
+sudo snap install spotify
+
+# vlc
+sudo snap install vlc
+
+# transmission
+sudo apt install transmission-gtk
+
+# screenfetch
+sudo apt install screenfetch
