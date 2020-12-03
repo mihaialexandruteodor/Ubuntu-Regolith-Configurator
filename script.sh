58eed5319d2bc52115564599ed5c741338bfe284
Mihai Alexandru Teodor <40050793+mihaialexandruteodor@users.noreply.github.com>
2020-12-03 19:19:36 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,15 +5,6 @@ sudo apt-get install git
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt install regolith-desktop i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
 
-## polybar
-
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
-cd Downloads
-sudo apt-get install build-essential
-sudo apt-get install libjsoncpp-dev 
-git clone https://github.com/jaagr/polybar.git
-cd polybar && ./build.sh
-cd
 
 ## Allacritty
 
